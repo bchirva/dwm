3b274de602bd29aaa3afc63dc413635c576aae2f
b_chirva <bchirva@gmail.com>
2026-03-08 15:54:03 +0500

    Custom patch for nerdiconed tags
    - Show tags until last active tag
    - Display icons instead of tag names
    - Remove "occupied" boxes

--- config.def.h
+++ config.def.h
@@ -27,6 +27,8 @@ static char *colors[][3] = {
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char tagiconoutline[] = "󰨝";
+static const char tagiconfill[] = "󰕮";
 
 static const Rule rules[] = {
 	/* xprop(1):
--- dwm.c
+++ dwm.c
@@ -752,10 +752,9 @@ void
 drawbar(Monitor *m)
 {
 	int x, w, tw = 0;
-	int boxs = drw->fonts->h / 9;
-	int boxw = drw->fonts->h / 6 + 2;
-	unsigned int i, occ = 0, urg = 0;
+	unsigned int i, occ = 0, urg = 0, used = 0;
 	Client *c;
+    const char* icon = NULL;
 
 	if (!m->showbar)
 		return;
@@ -787,28 +786,29 @@ drawbar(Monitor *m)
 		if (c->isurgent)
 			urg |= c->tags;
 	}
+
+    for (i = 0; i < LENGTH(tags); i++) {
+        if (m->tagset[m->seltags] & 1 << i || occ & 1 << i) {
+            used = i + 1;
+        }
+    }
 	x = 0;
-	for (i = 0; i < LENGTH(tags); i++) {
-		w = TEXTW(tags[i]);
+	for (i = 0; i < used; i++) {
+        icon = (m->tagset[m->seltags] & 1 << i || occ & 1 << i) 
+            ? tagiconfill
+            : tagiconoutline;
+
+		w = TEXTW(icon);
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
-		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
-		if (occ & 1 << i)
-			drw_rect(drw, x + boxs, boxs, boxw, boxw,
-				m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
-				urg & 1 << i);
+		drw_text(drw, x, 0, w, bh, lrpad / 2, icon, urg & 1 << i);
 		x += w;
 	}
-	w = TEXTW(m->ltsymbol);
-	drw_setscheme(drw, scheme[SchemeNorm]);
-	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
 
 	if ((w = m->ww - tw - x) > bh) {
 		if (m->sel) {
 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
 			drw_text(drw, x, 0, w, bh, lrpad / 2 + (m->sel->icon ? m->sel->icw + iconspacing : 0), m->sel->name, 0);
 			if (m->sel->icon) drw_pic(drw, x + lrpad / 2, (bh - m->sel->ich) / 2, m->sel->icw, m->sel->ich, m->sel->icon);
-			if (m->sel->isfloating)
-				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
 		} else {
 			drw_setscheme(drw, scheme[SchemeNorm]);
 			drw_rect(drw, x, 0, w, bh, 1, 1);
